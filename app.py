8e33607eaf859ab2563f60fb699160e27d6c5aeb
Anutosh Yadav <anutoshyadav@Anutoshs-MacBook-Air.local>
2025-08-05 17:22:18 +0530

    made changes in app.py to enable render

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def search():
 # --- Deploy Ready ---
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
