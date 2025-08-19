7f33d76ab9822ac7f234543a868c0b782bf0a5c1
Priyal Yadav <priyalyadav@Priyals-MacBook-Air.local>
2025-08-19 17:17:57 +0530

    changes made in app.py to connect to railway

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def search():
     return render_template("result.html", query=query, results=top_results)
 
 
-# --- Deploy Ready ---
+# --- Deploy Ready with Debug ---
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
